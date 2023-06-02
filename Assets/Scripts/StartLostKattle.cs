7a0f58bc6bcdfbb10fdf01a747e3c5115e8df813
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-06-02 22:29:26 +0900

    移動方法の修正、左タイルの修正、右タイルの作成

--- Assets/Scripts/StartLostKattle.cs
+++ Assets/Scripts/StartLostKattle.cs
@@ -9,10 +9,11 @@ public class StartLostKattle : MonoBehaviour
     [SerializeField] GameObject LostKattle;
     [SerializeField] public float move = 0.01f;
     string tile = "first";
+    string before;
     // Start is called before the first frame update
     void Start()
     {
-       
+        before = "forward";
     }
 
     // Update is called once per frame
@@ -27,7 +28,46 @@ public class StartLostKattle : MonoBehaviour
 
 
             case "left":
-                this.gameObject.transform.position = new Vector3(pos.x - move, pos.y, pos.z);
+                switch(before)
+                {
+                    case "forward":
+                        this.gameObject.transform.position = new Vector3(pos.x, pos.y + move, pos.z);
+                        break;
+
+                    case "right":
+                        this.gameObject.transform.position = new Vector3(pos.x + move, pos.y, pos.z);
+                        break;
+
+                    case "left":
+                        this.gameObject.transform.position = new Vector3(pos.x - move, pos.y, pos.z);
+                        break;
+
+                    case "down":
+                        this.gameObject.transform.position = new Vector3(pos.x, pos.y - move, pos.z);
+                        break;
+                }
+                break;
+
+
+            case "right":
+                switch (before)
+                {
+                    case "forward":
+                        this.gameObject.transform.position = new Vector3(pos.x, pos.y + move, pos.z);
+                        break;
+
+                    case "right":
+                        this.gameObject.transform.position = new Vector3(pos.x + move, pos.y, pos.z);
+                        break;
+
+                    case "left":
+                        this.gameObject.transform.position = new Vector3(pos.x - move, pos.y, pos.z);
+                        break;
+
+                    case "down":
+                        this.gameObject.transform.position = new Vector3(pos.x, pos.y - move, pos.z);
+                        break;
+                }
                 break;
         }
 
@@ -35,12 +75,60 @@ public class StartLostKattle : MonoBehaviour
         
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
-        //ç∂Ç…
-        if (collision.gameObject.name == "RotateLeftTile")
+        //ç∂Ç…ã»Ç™ÇÈ
+        if (collision.gameObject.tag == "Left tile")
         {
             tile = "left";
+
+            switch(before)
+            {
+                case "forward":
+                    before = "left";
+                    break;
+
+                case "right":
+                    before = "forward";
+                    break;
+
+                case "left":
+                    before = "down";
+                    break;
+
+                case "down":
+                    before = "right";
+                    break;
+
+
+            }
+        }
+
+        //âEÇ…ã»Ç™ÇÈ
+        if (collision.gameObject.tag == "Right tile")
+        {
+            tile = "left";
+
+            switch (before)
+            {
+                case "forward":
+                    before = "right";
+                    break;
+
+                case "right":
+                    before = "down";
+                    break;
+
+                case "left":
+                    before = "forward";
+                    break;
+
+                case "down":
+                    before = "left";
+                    break;
+
+
+            }
         }
     }
 }
