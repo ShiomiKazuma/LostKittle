e4dab35e9d163a10ac180e2c96d1b6bb764764e0
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-06-10 22:09:52 +0900

    UIに経過時間と現在のステージの表示を追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] int player_hp = 1;
     [SerializeField] GameObject canvas;
     string scean_name;
-    
+    GameObject stage_name;
+    GameObject time;
+    private float timer;
 
+    private void Start()
+    {
+        stage_name = GameObject.Find("StageName");
+        time = GameObject.Find ("Time");
+        timer = 0;
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Reset")) 
+        Text stage_name_text = stage_name.GetComponent<Text>();
+        stage_name_text.text = SceneManager.GetActiveScene().name;
+
+        Text time_text = time.GetComponent<Text>();
+        timer += Time.deltaTime;
+        time_text.text = timer.ToString("f2");
+
+        if (Input.GetButtonDown("Reset")) 
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
